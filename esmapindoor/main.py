61b8d08ded23f26b87396a8634e7e231b21724b4
Abelit <ychenid@live.com>
2020-02-26 21:21:25 +0800

    add project

---
+++ esmapindoor/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+
+# 创建flask实例对象
+app = Flask(__name__,
+            template_folder='./templates',
+            static_folder='./static',
+            static_url_path=''
+            )
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/event")
+def event():
+    return render_template("events/event-nodeclick.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
