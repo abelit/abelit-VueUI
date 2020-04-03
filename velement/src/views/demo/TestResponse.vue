26131ba0f76d4c1e60b76329fa3db50fdb0e66bc
abelit <ychenid@live.com>
2020-04-03 17:58:43 +0800

    update velement

--- velement/src/views/demo/TestResponse.vue
+++ velement/src/views/demo/TestResponse.vue
@@ -66,6 +66,13 @@
         </el-upload>
       </el-col>
     </el-row>
+    <el-row style="padding-top: 10px;">
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column prop="date" label="日期" width="180"></el-table-column>
+        <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+        <el-table-column prop="address" label="地址"></el-table-column>
+      </el-table>
+    </el-row>
   </div>
 </template>
 
@@ -92,6 +99,28 @@ export default {
         name: "food2.jpeg",
         url:
           "https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100"
+      }
+    ],
+    tableData: [
+      {
+        date: "2016-05-02",
+        name: "王小虎",
+        address: "上海市普陀区金沙江路 1518 弄"
+      },
+      {
+        date: "2016-05-04",
+        name: "王小虎",
+        address: "上海市普陀区金沙江路 1517 弄"
+      },
+      {
+        date: "2016-05-01",
+        name: "王小虎",
+        address: "上海市普陀区金沙江路 1519 弄"
+      },
+      {
+        date: "2016-05-03",
+        name: "王小虎",
+        address: "上海市普陀区金沙江路 1516 弄"
       }
     ]
   }),
